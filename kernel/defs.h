ae747f54634309f643ae1310070e718f54f939a8
car70scep <cgcepeda@miners.utep.edu>
2023-11-16 15:59:58 -0700

    Task 1

--- kernel/defs.h
+++ kernel/defs.h
@@ -105,6 +105,11 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             procinfo(uint64);
+struct          mmr_list* get_mmr_list(int);
+int             alloc_mmr_listid(void);
+void            dealloc_mmr_listid(int);
+void            mmrlistinit(void);
+
 
 // swtch.S
 void            swtch(struct context*, struct context*);
@@ -171,6 +176,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             mapvpages(pagetable_t, uint64, uint64);
 
 // plic.c
 void            plicinit(void);
