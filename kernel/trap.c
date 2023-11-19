827d2376e4b3dc50dc1c98423a770231ada0202a
car70scep <ccepeda122@gmail.com>
2023-11-19 13:43:16 -0700

    test

--- kernel/trap.c
+++ kernel/trap.c
@@ -33,56 +33,6 @@ trapinithart(void)
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
 //
-// void
-// usertrap(void)
-// {
-//   int which_dev = 0;
-
-//   if((r_sstatus() & SSTATUS_SPP) != 0)
-//     panic("usertrap: not from user mode");
-
-//   // send interrupts and exceptions to kerneltrap(),
-//   // since we're now in the kernel.
-//   w_stvec((uint64)kernelvec);
-
-//   struct proc *p = myproc();
-  
-//   // save user program counter.
-//   p->trapframe->epc = r_sepc();
-  
-//   if(r_scause() == 8){
-//     // system call
-
-//     if(p->killed)
-//       exit(-1);
-
-//     // sepc points to the ecall instruction,
-//     // but we want to return to the next instruction.
-//     p->trapframe->epc += 4;
-
-//     // an interrupt will change sstatus &c registers,
-//     // so don't enable until done with those registers.
-//     intr_on();
-
-//     syscall();
-//   } else if((which_dev = devintr()) != 0){
-//     // ok
-//   } else {
-//     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
-//     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
-//     p->killed = 1;
-//   }
-
-//   if(p->killed)
-//     exit(-1);
-
-//   // give up the CPU if this is a timer interrupt.
-//   if(which_dev == 2)
-//     yield();
-
-//   usertrapret();
-// }
-
 void
 usertrap(void)
 {
@@ -115,33 +65,6 @@ usertrap(void)
     intr_on();
 
     syscall();
-    
-
-  } else if(r_scause()==13||r_scause()==15){
-     uint64 fault_addr = r_stval();
-    for(int i = 0; i < MAX_MMR; i++){
-    if(p->mmr[i].valid == 1){
-      if((fault_addr >= p->mmr[i].addr) && (fault_addr < p->mmr[i].addr + p->mmr[i].length)){
-        if(r_scause() == 13 && (p->mmr[i].prot & PTE_R)){ 
-          //Load Faults - Read
-          memset(kalloc(), 0, PGSIZE);
-          if(mappages(p->pagetable, PGROUNDDOWN(fault_addr), PGSIZE, (uint64)kalloc(), p->mmr[i].prot | PTE_U) < 0){
-            p->killed = 1;
-            exit(-1);
-          }
-        }
-        if(r_scause() == 15 && (p->mmr[i].prot & PTE_W)){ 
-          //Store Faults - Write
-          memset(kalloc(), 0, PGSIZE);
-          if(mappages(p->pagetable, PGROUNDDOWN(fault_addr), PGSIZE, (uint64)kalloc(), p->mmr[i].prot | PTE_U) < 0){
-            p->killed = 1;
-            exit(-1);
-                                        }
-          }
-        }
-      }
-    }
-
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
@@ -153,27 +76,104 @@ usertrap(void)
   if(p->killed)
     exit(-1);
 
-  if(which_dev == 2){
-    p->cputime++; 
-    p->tsticks++; 
-  
-
-   if(p->tsticks >= timeslice(p->priority)){
-        if(p->priority == HIGH){
-           p->priority = MEDIUM;
-           yield();
-         }else if(p->priority == MEDIUM){
-           p->priority = LOW;
-           yield();
-         }else{
-           p->priority = LOW;
-           yield();
-         }
-    }
-  }
+  // give up the CPU if this is a timer interrupt.
+  if(which_dev == 2)
+    yield();
 
   usertrapret();
 }
+
+// void
+// usertrap(void)
+// {
+//   int which_dev = 0;
+
+//   if((r_sstatus() & SSTATUS_SPP) != 0)
+//     panic("usertrap: not from user mode");
+
+//   // send interrupts and exceptions to kerneltrap(),
+//   // since we're now in the kernel.
+//   w_stvec((uint64)kernelvec);
+
+//   struct proc *p = myproc();
+  
+//   // save user program counter.
+//   p->trapframe->epc = r_sepc();
+  
+//   if(r_scause() == 8){
+//     // system call
+
+//     if(p->killed)
+//       exit(-1);
+
+//     // sepc points to the ecall instruction,
+//     // but we want to return to the next instruction.
+//     p->trapframe->epc += 4;
+
+//     // an interrupt will change sstatus &c registers,
+//     // so don't enable until done with those registers.
+//     intr_on();
+
+//     syscall();
+    
+
+//   } else if(r_scause()==13||r_scause()==15){
+//      uint64 fault_addr = r_stval();
+//     for(int i = 0; i < MAX_MMR; i++){
+//     if(p->mmr[i].valid == 1){
+//       if((fault_addr >= p->mmr[i].addr) && (fault_addr < p->mmr[i].addr + p->mmr[i].length)){
+//         if(r_scause() == 13 && (p->mmr[i].prot & PTE_R)){ 
+//           //Load Faults - Read
+//           memset(kalloc(), 0, PGSIZE);
+//           if(mappages(p->pagetable, PGROUNDDOWN(fault_addr), PGSIZE, (uint64)kalloc(), p->mmr[i].prot | PTE_U) < 0){
+//             p->killed = 1;
+//             exit(-1);
+//           }
+//         }
+//         if(r_scause() == 15 && (p->mmr[i].prot & PTE_W)){ 
+//           //Store Faults - Write
+//           memset(kalloc(), 0, PGSIZE);
+//           if(mappages(p->pagetable, PGROUNDDOWN(fault_addr), PGSIZE, (uint64)kalloc(), p->mmr[i].prot | PTE_U) < 0){
+//             p->killed = 1;
+//             exit(-1);
+//                                         }
+//           }
+//         }
+//       }
+//     }
+
+//   } else if((which_dev = devintr()) != 0){
+//     // ok
+//   } else {
+//     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+//     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+//     p->killed = 1;
+//   }
+
+//   if(p->killed)
+//     exit(-1);
+
+//   if(which_dev == 2){
+//     p->cputime++; 
+//     p->tsticks++; 
+  
+
+//    if(p->tsticks >= timeslice(p->priority)){
+//         if(p->priority == HIGH){
+//            p->priority = MEDIUM;
+//            yield();
+//          }else if(p->priority == MEDIUM){
+//            p->priority = LOW;
+//            yield();
+//          }else{
+//            p->priority = LOW;
+//            yield();
+//          }
+//     }
+//   }
+
+//   usertrapret();
+// }
 
 //
 // return to user space
@@ -222,6 +222,35 @@ usertrapret(void)
 
 // interrupts and exceptions from kernel code go here via kernelvec,
 // on whatever the current kernel stack is.
+// void 
+// kerneltrap()
+// {
+//   int which_dev = 0;
+//   uint64 sepc = r_sepc();
+//   uint64 sstatus = r_sstatus();
+//   uint64 scause = r_scause();
+  
+//   if((sstatus & SSTATUS_SPP) == 0)
+//     panic("kerneltrap: not from supervisor mode");
+//   if(intr_get() != 0)
+//     panic("kerneltrap: interrupts enabled");
+
+//   if((which_dev = devintr()) == 0){
+//     printf("scause %p\n", scause);
+//     printf("sepc=%p stval=%p\n", r_sepc(), r_stval());
+//     panic("kerneltrap");
+//   }
+
+//   // give up the CPU if this is a timer interrupt.
+//   if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
+//     yield();
+
+//   // the yield() may have caused some traps to occur,
+//   // so restore trap registers for use by kernelvec.S's sepc instruction.
+//   w_sepc(sepc);
+//   w_sstatus(sstatus);
+// }
+
 void 
 kerneltrap()
 {
@@ -242,8 +271,26 @@ kerneltrap()
   }
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
-    yield();
+  if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING){
+   // yield();
+    myproc()->cputime++; //Christian Gomez: Increment CPU time
+    myproc()->tsticks++; //Christian Gomez: Increment Sticks
+
+    //Christian Gomez Task 4 -> kerneltrap() method
+    if(myproc()->tsticks >= timeslice(myproc()->priority)){
+       if(myproc()->priority == HIGH){
+         myproc()->priority = MEDIUM;
+         yield();
+       }else if(myproc()->priority == MEDIUM){
+          myproc()->priority = LOW;
+          yield();
+        }else{
+           myproc()->priority = LOW;
+           yield();
+         }
+    }//if
+
+  }
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
