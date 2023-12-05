2bf561b84730c5e5e09bd25ac9173c532210ca61
car70scep <ccepeda122@gmail.com>
2023-12-05 09:14:43 -0700

    test

--- kernel/trap.c
+++ kernel/trap.c
@@ -5,6 +5,7 @@
 #include "spinlock.h"
 #include "proc.h"
 #include "defs.h"
+#include "stat.h"
 
 struct spinlock tickslock;
 uint ticks;
@@ -27,54 +28,142 @@ trapinithart(void)
   w_stvec((uint64)kernelvec);
 }
 
+// void
+// usertrap(void)
+// {
+//   int which_dev = 0;
+  
+//   int newsz = myproc()->sz;
+//   if((r_sstatus() & SSTATUS_SPP) != 0)
+//     panic("usertrap: not from user mode");
+//   w_stvec((uint64)kernelvec);
+//   struct proc *p = myproc();
+//   p->trapframe->epc = r_sepc();
+//   if(r_scause() == 8){
+//     if(p->killed)
+//       exit(-1);
+//     p->trapframe->epc += 4;
+//     intr_on();
+//  syscall();
+//   } else if(r_scause() == 0xf || r_scause() == 13){
+    
+//     if(r_stval() > newsz){
+//       char *mem = kalloc();
+//       if (mem == 0) {
+//         printf("Out of memory\n");
+//         p->killed = 1;
+//       } else {
+//         memset(mem, 0, PGSIZE);
+//         if (mappages(p->pagetable, PGROUNDDOWN(r_stval()), PGSIZE, (uint64)mem, PTE_W | PTE_X | PTE_R | PTE_U) != 0) {
+//           printf("Failed to map memory\n");
+//           kfree(mem); 
+//           p->killed = 1;
+//         }
+//       }
+//     }else{
+//       printf("Invalid memory access at address %p\n", r_stval());
+//       p->killed = 1;
+//     }
+//   }else{
+//     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+//     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+//     p->killed = 1;
+//   }
+//   if(p->killed)
+//     exit(-1);
+//   if(which_dev == 2)
+//     yield();
+
+//   usertrapret();
+// }
+
 void
 usertrap(void)
 {
   int which_dev = 0;
-  
-  int newsz = myproc()->sz;
+
   if((r_sstatus() & SSTATUS_SPP) != 0)
     panic("usertrap: not from user mode");
+
+  // send interrupts and exceptions to kerneltrap(),
+  // since we're now in the kernel.
   w_stvec((uint64)kernelvec);
+
   struct proc *p = myproc();
+
+  // save user program counter.
   p->trapframe->epc = r_sepc();
+
   if(r_scause() == 8){
+    // system call
+
     if(p->killed)
       exit(-1);
+
+    // sepc points to the ecall instruction,
+    // but we want to return to the next instruction.
     p->trapframe->epc += 4;
+
+    // an interrupt will change sstatus &c registers,
+    // so don't enable until done with those registers.
     intr_on();
- syscall();
-  } else if(r_scause() == 0xf || r_scause() == 13){
+
+    syscall();
+  } else if((which_dev = devintr()) != 0){
+    // ok
+
     
-    if(r_stval() < newsz){
-      char *mem = kalloc();
-      if (mem == 0) {
-        printf("Out of memory\n");
-        p->killed = 1;
-      } else {
-        memset(mem, 0, PGSIZE);
-        if (mappages(p->pagetable, PGROUNDDOWN(r_stval()), PGSIZE, (uint64)mem, PTE_W | PTE_X | PTE_R | PTE_U) != 0) {
-          printf("Failed to map memory\n");
-          kfree(mem); 
+  } else if(r_scause() == 13 || r_scause() == 15){
+      // Check mapped region protection permits operation
+      if(r_stval() >= p->sz){
+        for(int i=0; i<MAX_MMR; i++){
+          if(p->mmr[i].valid && p->mmr[i].addr < r_stval() && p->mmr[i].addr+p->mmr[i].length > r_stval()){
+            // Page fault load
+            if(r_scause() == 13){
+              // Read permission
+              if((p->mmr[i].prot & PROT_READ) == 0){
+                p->killed = 1;
+                exit(-1);
+              }
+            }
+            // Page fault store
+            if(r_scause() == 15){
+              // Write permission
+              if((p->mmr[i].prot & PROT_WRITE) == 0){
+                p->killed = 1;
+                exit(-1);
+              }
+            }
+          }
+        }
+      }
+      // Allocate a physical memory frame
+      void *physical_frame = kalloc();
+      if(physical_frame){
+        // Rounds down fault address to a page boundary
+        if(mappages(p->pagetable, PGROUNDDOWN(r_stval()), PGSIZE, (uint64)physical_frame, (PTE_R | PTE_W | PTE_X | PTE_U)) < 0){
+          kfree(physical_frame);
           p->killed = 1;
         }
       }
-    }else{
-      printf("Invalid memory access at address %p\n", r_stval());
-      p->killed = 1;
-    }
-  }else{
+  } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
   }
+
   if(p->killed)
     exit(-1);
+
+  // give up the CPU if this is a timer interrupt.
   if(which_dev == 2)
     yield();
 
   usertrapret();
 }
+
+
+
 
 // void
 // usertrap(void)
